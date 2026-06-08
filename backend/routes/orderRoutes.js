f9f5cde4349ae63ed717fc6afd9f8e8087c15eba
Ritik Kanoujiya <ritikkanoujiya04@gmail.com>
2026-06-08 10:42:25 +0530

    fix order model filename case issue

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const Order = require("../models/Order");
+const Order = require("../models/Order.js");
 
 // PLACE ORDER
 router.post("/", async (req, res) => {
